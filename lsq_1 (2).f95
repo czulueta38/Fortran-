34015b4fe7184746fa59f38d927830085cb82613
czulueta38 <60370633+czulueta38@users.noreply.github.com>
2022-07-15 10:56:10 -0400

    Update lsq_1 (2).f95

--- lsq_1 (2).f95
+++ lsq_1 (2).f95
@@ -18,13 +18,13 @@ program exponent
     open(42,file='Edata.txt')
 
     !Initializing Variables for equations
-    Sxx=0
-    Sxy=0
-    xAverage=0
-    yAverage=0
+    Sxx=0.0
+    Sxy=0.0
+    xAverage=0.0
+    yAverage=0.0
 
       !Do loop that iterates the 200 points
-     do i=1,200
+     do i=1.0,200.0
         read(42,*) x,y !Reading in x and y
         y=log(y) !log of y putting it back into y
         !Equations for Exponential equation turned to linear equation
