14c75b117a2155d7f974471086709eaae8d0e270
czulueta38 <60370633+czulueta38@users.noreply.github.com>
2022-07-15 10:21:31 -0400

    Add files via upload

---
+++ BasicProgram (2).f95
@@ -0,0 +1,34 @@
+program BasicProgram
+! Cariza Zulueta
+! CDS:251
+! Assignment 1
+!09/01/2021
+!This is assignment 1 which is a program that prints out my name,email,course number, assignment, and due date
+
+implicit none
+
+!Variables declared 
+
+character*22 Name
+character*40 Email
+character*30 Course
+character*42 Assignment 
+character*80 Date
+
+
+!Executable Statements 
+Name='Cariza Zulueta'
+Email='carizamz@yahoo.com'
+Course='CDS 251'
+Assignment='Assignment 1'
+Date='Due Date: September 1st, 2021'
+
+!Print Statements 
+print*,Name
+print*,Email
+print*,Course
+print*,Assignment
+print*,Date
+
+!End Program 
+end program BasicProgram
